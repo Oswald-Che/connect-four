a89d120d2ba6172c0622711e449823b1ff855ff5
Oswald-Che <cheoswald@yahoo.com>
2024-06-06 00:09:03 +0100

    add method to update neighbours with written test

--- lib/node.rb
+++ lib/node.rb
@@ -1,15 +1,25 @@
 # class to store information on game pieces
 class Node
-  def initialize(data = nil, colour = nil)
-    @data = data
+  def initialize(pos, colour = nil)
+    @pos = pos
     @colour = colour
     @symbol = unicode_symbol(colour)
-    @neighbours = []
+    @neighbours = update_neighbours(@pos)
   end
 
   def unicode_symbol(colour)
     colour == 'WHITE' ? "\u26AA" : "\u26AB"
   end
 
+  def update_neighbours(data, array = [])
+    deltas.each do |item|
+      ar = [data, item].transpose.map(&:sum)
+      array << (ar.all? { |i| i.between?(0, 7) } ? ar : nil)
+    end
+    array
+  end
 
+  def deltas
+    [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+  end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -7,7 +7,7 @@ describe Node do
 
   describe '#unicode_symbol' do
     context 'when colour is white' do
-      subject(:node_symbol) { described_class.new }
+      subject(:node_symbol) { described_class.new([0, 0]) }
 
       it 'return the appropriate unicode symbol' do
         symbol = "\u26AA"
@@ -16,12 +16,47 @@ describe Node do
       end
     end
     context 'when colour is black' do
-      subject(:node_symbol) { described_class.new }
+      subject(:node_symbol) { described_class.new([0, 0]) }
 
       it 'return the appropriate unicode symbol' do
         symbol = "\u26AB"
         result = node_symbol.unicode_symbol('BLACK')
         expect(result).to eq(symbol)
+      end
+    end
+  end
+
+  describe '#update_neighbours' do
+    context 'when peice position is 0, 0' do
+      subject(:node_neighbour) { described_class.new([0, 0]) }
+
+      it 'returns positions of its neighbours' do
+        pos = [0, 0]
+        neighbours = [[0, 1], [1, 0], [1, 1]]
+        result = node_neighbour.update_neighbours(pos).compact
+        expect(result).to eq(neighbours)
+      end
+    end
+
+    context 'when peice position is 7, 4' do
+      subject(:node_neighbour) { described_class.new([7, 4]) }
+
+      it 'returns positions of its neighbours' do
+        pos = [7, 4]
+        neighbours = [[6, 3], [6, 4], [6, 5], [7, 3], [7, 5]]
+        result = node_neighbour.update_neighbours(pos).compact
+        expect(result).to eq(neighbours)
+      end
+    end
+
+    context 'when peice position is 3, 3' do
+      subject(:node_neighbour) { described_class.new([3, 3]) }
+
+      it 'returns positions of its neighbours' do
+        pos = [3, 3]
+        neighbours = [[2, 2], [2, 3], [2, 4], [3, 2], [3, 4], [4, 2], [4, 3], [4, 4]]
+        result = node_neighbour.update_neighbours(pos).compact
+        expect(result).to eq(neighbours)
       end
     end
   end
